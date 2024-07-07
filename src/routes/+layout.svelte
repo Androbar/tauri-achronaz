f944b53b6da03d526a9af9a189f195b44a0e3a16
Androbar <androbar115@gmail.com>
2024-07-07 13:03:50 -0300

    Prepare fo windows compile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
   import { browser } from '$app/environment';
 	import { onMount } from "svelte";
 
+  export const prerender = true
+  export const ssr = false;
+
   onMount(() => {
     if (browser) {
       import('../stores/broadcastChannel').then(() => {
